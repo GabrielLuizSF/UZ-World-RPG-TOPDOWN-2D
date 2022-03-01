33ce7c71c63af13569b10161de6c05f0bef8c98b
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-03-01 09:58:54 -0300

    code

---
+++ World/Assets/Scripts/PlayerController7PC.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController7PC : MonoBehaviour
+{
+
+    private Rigidbody2D myRB;
+        [SerializeField]
+        private float speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new  Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+    }
+}
