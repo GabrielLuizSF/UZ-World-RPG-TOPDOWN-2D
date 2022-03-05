315ccaaa7f4ff5d70c3a052d4432ca3437bea8a6
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-03-05 12:06:38 -0300

    Update PC

--- World/Assets/Scripts/PlayerController7PC.cs
+++ World/Assets/Scripts/PlayerController7PC.cs
@@ -6,25 +6,26 @@ public class PlayerController7PC : MonoBehaviour
 {
 
     private Rigidbody2D myRB;
+    private Animator myAnim;
         [SerializeField]
         private float speed;
-
+ 
 
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
-
+        myAnim = GetComponent<Animator>();
     
     }
-    
 
     // Update is called once per frame
     void Update()
     {
         myRB.velocity = new  Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
-
+       myAnim.SetFloat("moveX",myRB.velocity.x);
+       myAnim.SetFloat("moveY",myRB.velocity.y);
 
     }
 }
